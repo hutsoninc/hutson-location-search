f35f2d7d605eba98e8e8c35a7225fa94ae1bfc17
AustinLeeGordon <gordon.austin2@gmail.com>
2021-05-27 11:39:14 -0500

    Added Michigan locations

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,52 +1,38 @@
 function debounce(func, wait, immediate) {
-	var timeout;
-	return function () {
-		var context = this,
-			args = arguments;
-		var later = function () {
-			timeout = null;
-			if (!immediate) {
-				func.apply(context, args);
-			}
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait || 200);
-		if (callNow) {
-			func.apply(context, args);
-		}
-	};
-};
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) {
+                func.apply(context, args);
+            }
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait || 200);
+        if (callNow) {
+            func.apply(context, args);
+        }
+    };
+}
 
 function metersToMiles(meters) {
-	return (meters / 1609.344).toFixed(2);
+    return (meters / 1609.344).toFixed(2);
 }
 
 function secondsToTime(seconds) {
-	let h, m;
-	let out = [];
-	if (seconds / 3600 > 1) {
-		h = Math.floor(seconds / 3600);
-		seconds = seconds - h * 3600;
-		out.push(h + 'h');
-	}
-	m = Math.floor(seconds / 60);
-	out.push(m + 'm');
-	return out.join(' ');
+    let h, m;
+    let out = [];
+    if (seconds / 3600 > 1) {
+        h = Math.floor(seconds / 3600);
+        seconds = seconds - h * 3600;
+        out.push(h + 'h');
+    }
+    m = Math.floor(seconds / 60);
+    out.push(m + 'm');
+    return out.join(' ');
 }
 
-function formatPhoneNumber(phoneNumberString) {
-	let cleaned = ('' + phoneNumberString).replace(/\D/g, '');
-	let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
-	if (match) {
-		return '(' + match[1] + ') ' + match[2] + '-' + match[3];
-	}
-	return null;
-}
-
-export {
-	debounce,
-	metersToMiles,
-	secondsToTime,
-	formatPhoneNumber
-};
+export { debounce, metersToMiles, secondsToTime };
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import fetch from 'isomorphic-fetch';
-import hutsonData from 'hutson-location-data';
-import { debounce, metersToMiles, secondsToTime, formatPhoneNumber } from './helpers';
+import locationsData from './locations';
+import { debounce, metersToMiles, secondsToTime } from './helpers';
 
 require('dotenv').config();
 
@@ -9,17 +9,24 @@ const config = {
     baseURL: 'https://api.mapbox.com',
     geocodePath: '/geocoding/v5/mapbox.places/',
     matrixPath: '/directions-matrix/v1/mapbox/driving/',
-    hutsonCoords: []
+    storeCoordinateGroups: [[]],
 };
 
-let data = hutsonData;
-
-data.map(loc => {
-    config.hutsonCoords.push(loc.coordinates.join(','));
-    loc.phone = formatPhoneNumber(loc.phone);
-    loc.partsPhone = formatPhoneNumber(loc.partsPhone);
-    loc.servicePhone = formatPhoneNumber(loc.servicePhone);
-    return loc;
+// Add store coordinates to groups in config
+// Max 25 coordinates per group (MapBox limit). Leaving 1 spot open for the source coordinates.
+locationsData.forEach((loc) => {
+    // Check if group is full
+    if (
+        config.storeCoordinateGroups[config.storeCoordinateGroups.length - 1]
+            .length === 24
+    ) {
+        // Start a new group
+        config.storeCoordinateGroups.push([]);
+    }
+    const groupNumber = config.storeCoordinateGroups.length - 1;
+    config.storeCoordinateGroups[groupNumber].push(
+        `${loc.geolocation.lng},${loc.geolocation.lat}`
+    );
 });
 
 export default class extends React.Component {
@@ -34,8 +41,8 @@ export default class extends React.Component {
             success: '',
             suggestions: [],
             suggestionsHidden: true,
-            locations: data
-        }
+            locations: locationsData,
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.onSearch = this.onSearch.bind(this);
@@ -50,7 +57,7 @@ export default class extends React.Component {
     }
     handleChange(e) {
         this.setState({
-            search: e.target.value
+            search: e.target.value,
         });
         if (e.target.value) {
             this.fetchSuggestions();
@@ -59,14 +66,14 @@ export default class extends React.Component {
     hideSuggestions() {
         if (!this.state.suggestionsHidden) {
             this.setState({
-                suggestionsHidden: true
+                suggestionsHidden: true,
             });
         }
     }
     showSuggestions() {
         if (this.state.suggestionsHidden) {
             this.setState({
-                suggestionsHidden: false
+                suggestionsHidden: false,
             });
         }
     }
@@ -76,42 +83,66 @@ export default class extends React.Component {
     updateWithSuggestion(e) {
         if (this.state.search === e.target.innerHTML) return;
 
-        let suggestionObj = this.state.suggestions.find(suggestion => suggestion.name.toLowerCase() === e.target.innerHTML.toLowerCase());
+        let suggestionObj = this.state.suggestions.find(
+            (suggestion) =>
+                suggestion.name.toLowerCase() ===
+                e.target.innerHTML.toLowerCase()
+        );
 
         this.setState({
             search: e.target.innerHTML,
             searchCoords: suggestionObj.coords,
-            suggestionsHidden: true
+            suggestionsHidden: true,
         });
 
         this.getMatrix(suggestionObj.coords);
     }
     async getMatrix(searchCoords) {
-        let searchURL = config.baseURL + config.matrixPath + searchCoords.join(',') + ';' + config.hutsonCoords.join(';') + '/?access_token=' + process.env.REACT_APP_MAPBOX_TOKEN + '&annotations=duration,distance&sources=0';
-        let res, body;
+        const output = [];
+        for (let i = 0; i < config.storeCoordinateGroups.length; i++) {
+            const searchURL =
+                config.baseURL +
+                config.matrixPath +
+                searchCoords.join(',') +
+                ';' +
+                config.storeCoordinateGroups[i].join(';') +
+                '/?access_token=' +
+                process.env.REACT_APP_MAPBOX_TOKEN +
+                '&annotations=duration,distance&sources=0';
 
-        try {
-            res = await fetch(searchURL, {
-                method: `get`
-            });
-        } catch (err) {
-            return this.onError(`Connection error`);
+            await fetch(searchURL, {
+                method: 'GET',
+            })
+                .then((res) => {
+                    if (res.status !== 200) {
+                        this.onError(`Error: ${res.statusText}`);
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    output.push(data);
+                })
+                .catch((err) => {
+                    this.onError(`Error: ${err}`);
+                });
         }
-
-        if (res.status !== 200) {
-            return this.onError(`Error: ${res.statusText}`);
-        }
-
-        try {
-            body = await res.json();
-        } catch (err) {
-            return this.onError(`Error parsing JSON`);
-        }
-
-        this.updateDistances(body);
+        // Combine responses
+        const data = output.reduce((acc, obj) => {
+            // Remove source from each
+            obj.distances[0].shift();
+            obj.durations[0].shift();
+            obj.destinations.shift();
+            // Add them
+            acc.distances[0].push(...obj.distances[0]);
+            acc.durations[0].push(...obj.durations[0]);
+            acc.destinations.push(...obj.destinations);
+            return acc;
+        });
+        console.log(data);
+        this.updateDistances(data);
     }
     updateDistances(body) {
-        let updatedLocations = data;
+        const updatedLocations = [...locationsData];
 
         for (let i = 0; i < updatedLocations.length; i++) {
             let time = body.durations[0][i + 1];
@@ -120,9 +151,7 @@ export default class extends React.Component {
             updatedLocations[i].distance = distance;
         }
 
-        let sortedLocations = Array.from(updatedLocations);
-
-        sortedLocations.sort((a, b) => {
+        updatedLocations.sort((a, b) => {
             if (a.duration < b.duration) {
                 return -1;
             }
@@ -133,21 +162,23 @@ export default class extends React.Component {
         });
 
         this.setState({
-            locations: sortedLocations
+            locations: updatedLocations,
         });
-
     }
     async onSearch() {
-
-        let searchURL = config.baseURL + config.geocodePath + encodeURIComponent(this.state.search) + '.json?access_token=' + process.env.REACT_APP_MAPBOX_TOKEN;
+        let searchURL =
+            config.baseURL +
+            config.geocodePath +
+            encodeURIComponent(this.state.search) +
+            '.json?access_token=' +
+            process.env.REACT_APP_MAPBOX_TOKEN;
         let res, body;
 
         try {
             res = await fetch(searchURL, {
-                method: `get`
+                method: `get`,
             });
-        }
-        catch (err) {
+        } catch (err) {
             return this.onError(`Connection error`);
         }
 
@@ -157,15 +188,13 @@ export default class extends React.Component {
 
         try {
             body = await res.json();
-        }
-        catch (err) {
+        } catch (err) {
             return this.onError(`Error parsing JSON`);
         }
 
         this.onSuccess(body);
     }
     onSuccess(body) {
-
         if (!body.features[0]) return;
 
         let locations = [];
@@ -179,7 +208,7 @@ export default class extends React.Component {
         for (let i = 0; i < len; i++) {
             locations.push({
                 name: body.features[i].place_name,
-                coords: body.features[i].geometry.coordinates
+                coords: body.features[i].geometry.coordinates,
             });
         }
 
@@ -188,7 +217,7 @@ export default class extends React.Component {
             success: body,
             error: false,
             suggestions: locations,
-            suggestionsHidden: false
+            suggestionsHidden: false,
         });
     }
     onError(err) {
@@ -199,7 +228,7 @@ export default class extends React.Component {
         });
     }
     componentDidMount() {
-        document.addEventListener('click', d => {
+        document.addEventListener('click', (d) => {
             if (d.target.id === 'search' || d.target.class === 'search-item') {
                 this.showSuggestions();
             } else {
@@ -212,65 +241,103 @@ export default class extends React.Component {
             <div className="main">
                 <div className="search">
                     <div>
-                        <input type="text" id="search" className="search-bar" onClick={this.showSuggestions} onChange={this.handleChange} value={this.state.search} autoFocus placeholder="Search..." />
-                        {this.state.suggestions.length > 0 &&
-                            <ul className="suggestions-list" style={!this.state.suggestionsHidden ? { display: 'block' } : { display: 'none' }}>
-                                {this.state.suggestions.map((suggestion, key) => (
-                                    <li key={key} className="search-item" onClick={this.updateWithSuggestion}>{suggestion.name}</li>
-                                ))}
+                        <input
+                            type="text"
+                            id="search"
+                            className="search-bar"
+                            onClick={this.showSuggestions}
+                            onChange={this.handleChange}
+                            value={this.state.search}
+                            autoFocus
+                            placeholder="Search..."
+                        />
+                        {this.state.suggestions.length > 0 && (
+                            <ul
+                                className="suggestions-list"
+                                style={
+                                    !this.state.suggestionsHidden
+                                        ? { display: 'block' }
+                                        : { display: 'none' }
+                                }
+                            >
+                                {this.state.suggestions.map(
+                                    (suggestion, key) => (
+                                        <li
+                                            key={key}
+                                            className="search-item"
+                                            onClick={this.updateWithSuggestion}
+                                        >
+                                            {suggestion.name}
+                                        </li>
+                                    )
+                                )}
                             </ul>
-                        }
+                        )}
                     </div>
                 </div>
                 <div className="results">
                     <table className="results-table">
                         <tbody>
                             <tr>
-                                <th>
-                                    Location
-                                </th>
-                                <th>
-                                    Phone
-                                </th>
-                                <th>
-                                    Parts
-                                </th>
-                                <th>
-                                    Service
-                                </th>
-                                <th>
-                                    Drive Time
-                                </th>
+                                <th>Location</th>
+                                <th>Phone</th>
+                                <th>After Hours Parts</th>
+                                <th>After Hours Service</th>
+                                <th>Drive Time</th>
                             </tr>
                             {this.state.locations.map((location, key) => (
                                 <tr key={key}>
                                     <td>
                                         <div className="location-header">
-                                            <h3>{location.title}</h3>
-                                            {location.distance &&
+                                            <h3>
+                                                <a
+                                                    href={`https://www.hutsoninc.com/locations/${
+                                                        location.handle
+                                                    }/`}
+                                                >
+                                                    {location.title}
+                                                </a>
+                                            </h3>
+                                            {location.distance && (
                                                 <span>
-                                                    ({metersToMiles(location.distance)} miles)
+                                                    (
+                                                    {metersToMiles(
+                                                        location.distance
+                                                    )}{' '}
+                                                    miles)
                                                 </span>
-                                            }
+                                            )}
                                         </div>
-                                        <p>{location.address}, {location.city}, {location.state} {location.zip}</p>
+                                        <p className="location-address">
+                                            <a href={location.googleMapsLink}>
+                                                {location.address},{' '}
+                                                {location.city},{' '}
+                                                {location.state} {location.zip}
+                                            </a>
+                                        </p>
                                     </td>
                                     <td>
-                                        <p>{location.phone}</p>
+                                        <p>{location.phoneNumber}</p>
                                     </td>
                                     <td>
-                                        <p>{location.partsPhone || '-'}</p>
+                                        <p>
+                                            {location.partsPhoneNumber || '-'}
+                                        </p>
                                     </td>
                                     <td>
-                                        <p>{location.servicePhone || '-'}</p>
+                                        <p>
+                                            {location.servicePhoneNumber || '-'}
+                                        </p>
                                     </td>
                                     <td>
-                                        {location.duration &&
-                                            <p>{secondsToTime(location.duration)}</p>
-                                        }
-                                        {!location.duration &&
-                                            <p>-</p>
-                                        }
+                                        {location.duration && (
+                                            <p>
+                                                {secondsToTime(
+                                                    location.duration
+                                                )}
+                                            </p>
+                                        )}
+                                        {!location.duration && <p>-</p>}
                                     </td>
                                 </tr>
                             ))}
